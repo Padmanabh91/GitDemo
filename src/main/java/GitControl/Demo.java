b9f2a3810fed5c6429a2fa81a9cb0d536b35c509
Padmanabh <padmanabh0619@gmail.com>
2025-11-18 19:28:39 +0530

    A

--- src/main/java/GitControl/Demo.java
+++ src/main/java/GitControl/Demo.java
@@ -9,6 +9,10 @@ public class Demo {
 				System.out.println("Jira2");
 				System.out.println("Jira3");
 				
+				System.out.println("Jira4");
+				System.out.println("Jira5");
+				System.out.println("Jira6");
+				 
 
 			
 
