409e16e8d961eb730753f739dee3d28df652f6f0
Padmanabh <padmanabh0619@gmail.com>
2025-11-18 18:36:28 +0530

    Demo

---
+++ src/main/java/GitControl/Demo.java
@@ -0,0 +1,19 @@
+package GitControl;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+				
+				System.out.println("Jira1");
+				System.out.println("Jira2");
+				System.out.println("Jira3");
+				System.out.println("Jira4");
+				System.out.println("Jira5");
+				System.out.println("Jira6");
+
+			
+
+	}
+
+}
